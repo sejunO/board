8dd755501961dfa648e980b881c3094f84fd0947
sejunO <friendlyoh@gmail.com>
2025-01-01 23:42:58 +0900

    chore: initial draft of board-datasource module

--- board-datasource/db/src/main/java/com/sejun/board/jpa/BaseEntity.java
+++ board-datasource/db/src/main/java/com/sejun/board/jpa/BaseEntity.java
@@ -1,2 +1,26 @@
-package com.sejun.board.jpa;public class BaseEntity {
+package com.sejun.board.jpa;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
 }
--- board-datasource/db/src/main/java/com/sejun/board/jpa/board/BoardEntity.java
+++ board-datasource/db/src/main/java/com/sejun/board/jpa/board/BoardEntity.java
@@ -1,2 +1,29 @@
-package com.sejun.board.jpa.board;public class BoardEntity {
+package com.sejun.board.jpa.board;
+
+import com.sejun.board.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardEntity extends BaseEntity {
+    private String title;
+    private String content;
+
+    @Builder
+    public BoardEntity(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void updateContent(String newContent) {
+        if (newContent == null || newContent.isBlank()) {
+            throw new IllegalArgumentException("Content cannot be empty");
+        }
+        this.content = newContent;
+    }
 }
